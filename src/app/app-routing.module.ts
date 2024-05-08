14adb923641dddb461a84d88325cde0155ed875b
Jacopo Capudi <jacopo.capudi@euris.it>
2024-05-08 10:12:38 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes =  [
-  { path: 'home', component:  AppComponent },
+  { path: 'home', component:  HomePageComponent },
+  { path: 'users', component: UserComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
 
