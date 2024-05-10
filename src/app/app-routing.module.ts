0f720f7436eaff494ac888e8698eda5e6c3c3ab8
Jacopo Capudi <jacopo.capudi@euris.it>
2024-05-10 09:30:41 +0200

    update imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
-import { HomePageComponent } from './home-page/home-page.component';
+
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { UserComponent } from './components/user/user.component';
 
 const routes: Routes =  [
   { path: 'home', component:  HomePageComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
+
 import { HttpClientModule } from '@angular/common/http';
-import { HomePageComponent } from './home-page/home-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { UserComponent } from './components/user/user.component';
 
 
 @NgModule({
