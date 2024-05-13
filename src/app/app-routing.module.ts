634a335efb93100780b371442d4712d3b34f66b6
Jacopo Capudi <jacopo.capudi@euris.it>
2024-05-13 17:40:06 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { UserComponent } from './components/user/user.component';
+import { TrainingComponent } from './components/training/training.component';
+import { ExerciseComponent } from './components/exercise/exercise.component';
+import { DiaryComponent } from './components/diary/diary.component';
+import { WorkoutComponent } from './components/workout/workout.component';
 
 const routes: Routes =  [
   { path: 'home', component:  HomePageComponent },
   { path: 'users', component: UserComponent },
+  { path: 'trainings', component:  TrainingComponent },
+  { path: 'exercises', component: ExerciseComponent },
+  { path: 'diary', component: DiaryComponent },
+  { path: 'workout', component: WorkoutComponent },//devi passare parametro
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
 
